7baef3a2fca8e21ccf775eb7546f660b07bbacd0
Maria Pera <mariapera500@gmail.com>
2024-04-17 14:28:53 +0200

    change style and propTypes in component: Title

--- tours-react-app/src/components/Title.jsx
+++ tours-react-app/src/components/Title.jsx
@@ -1,9 +1,15 @@
-const Title = ({children}) => {
-  return (
+import PropTypes from 'prop-types'
+
+const Title = ({ children }) => {
+	return (
 		<div className='title'>
 			<h2>{children}</h2>
 			<div className='title-underline'></div>
 		</div>
 	)
+}
+
+Title.propTypes = {
+	children: PropTypes.string
 }
 export default Title
